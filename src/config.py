17541021b939a340f64705e23547638917ac1eb5
Viktor Lundqvist <vlundq@kth.se>
2024-07-18 18:04:04 -0700

    Added checkpoint interval

--- src/config.py
+++ src/config.py
@@ -1,8 +1,9 @@
 
 class Config:
-    n_tasks = 8 # 2^1 to 2^17
+    n_tasks = 8
 
     train_steps = 100
+    checkpoint_interval = 10
     batch_size = 256
     learning_rate = 1e-3
     weight_decay = 1e-2
--- src/train.py
+++ src/train.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 from model import GPT2
 from collect_data import BatchIterator
@@ -8,7 +9,11 @@ from config import Config
 
 if __name__ == "__main__":
     config = Config()
+
     model = GPT2(n_dims_in=config.dim + 1, n_positions=config.n_points, n_dims_out=1)
+
     train_loader = BatchIterator(config)
-    trainer = pl.Trainer(max_epochs=1, max_steps=config.train_steps, accelerator="gpu", devices="auto")
+    checkpoint_callback = ModelCheckpoint(every_n_train_steps=config.checkpoint_interval, save_top_k=-1)    
+
+    trainer = pl.Trainer(max_epochs=1, max_steps=config.train_steps, accelerator="gpu", devices="auto", callbacks=[checkpoint_callback])
     trainer.fit(model=model, train_dataloaders=train_loader)
