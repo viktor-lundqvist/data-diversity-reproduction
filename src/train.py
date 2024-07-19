6b9198114e09488e3e72a16868ca0f0cacad0803
Viktor Lundqvist <vlundq@kth.se>
2024-07-19 10:24:57 -0700

    Added wandblogger

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
 import wandb
 
 from model import GPT2
@@ -12,13 +13,20 @@ if __name__ == "__main__":
     wandb.login()
     config = Config()
 
-    model = GPT2(n_dims_in=config.dim + 1, n_positions=config.n_points, n_dims_out=1)
-
     train_loader = BatchIterator(config)
-    checkpoint_callback = ModelCheckpoint(every_n_train_steps=config.checkpoint_interval, save_top_k=-1)    
+    checkpoint_callback = ModelCheckpoint(every_n_train_steps=config.checkpoint_interval, save_top_k=-1)
+    wandb_logger = WandbLogger(log_model="all", project="Data Diversity Reproduction")
 
-    trainer = pl.Trainer(max_epochs=1, max_steps=config.train_steps, accelerator="gpu", devices="auto", callbacks=[checkpoint_callback])
+    trainer = pl.Trainer(
+        max_epochs=1,
+        max_steps=config.train_steps,
+        accelerator="gpu",
+        devices="auto",
+        callbacks=[checkpoint_callback],
+        logger=wandb_logger
+    )
 
+    model = GPT2(n_dims_in=config.dim + 1, n_positions=config.n_points, n_dims_out=1)
     config_dict = {key:value for key, value in Config.__dict__.items() if not key.startswith('__') and not callable(key)}
     run = wandb.init(
         # Set the project where this run will be logged
