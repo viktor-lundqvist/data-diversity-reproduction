e2bceaeab80538e67ffcd94bb0664195af16ba2f
Viktor Lundqvist <vlundq@kth.se>
2024-08-06 09:50:31 +0200

    Fixed mistake confusing sigma/sigma^2 in predictions. Improved validation graphs.

--- src/predictions.py
+++ src/predictions.py
@@ -3,11 +3,15 @@ import torch
 
 def oracle(batch):
     targets = torch.matmul(batch["xs"], batch["ws"][:,:,np.newaxis])
-    return torch.squeeze(targets, axis=2)
+    preds = torch.squeeze(targets, axis=2)
+    return preds
+
+def zero(batch):
+    return torch.zeros_like(batch['ys'])
 
 class Ridge:
-    def __init__(self, sigma2):
-        self.sigma2 = sigma2
+    def __init__(self, sigma):
+        self.sigma = sigma
 
     def __call__(self, batch):
         data = batch["xs"]
@@ -26,17 +30,16 @@ class Ridge:
         _, _, n_dims = X.shape
         XT = X.transpose(2, 1)  # batch_size x n_dims x i
         XT_Y = XT @ Y  # batch_size x n_dims x 1, @ should be ok (batched matrix-vector product)
-        ridge_matrix = torch.matmul(XT, X) + self.sigma2 * torch.eye(n_dims).to(X.get_device())  # batch_size x n_dims x n_dims
+        ridge_matrix = torch.matmul(XT, X) + self.sigma**2 * torch.eye(n_dims).to(X.get_device())  # batch_size x n_dims x n_dims
         # batch_size x n_dims x 1
         ws = torch.linalg.solve(ridge_matrix.float(), XT_Y.float())
         pred = test_x @ ws  # @ should be ok (batched row times column)
         return pred[:, 0, 0]
 
-
 class DiscreteMMSE:
-    def __init__(self, task_pool, sigma2):
+    def __init__(self, task_pool, sigma):
         self.task_pool = task_pool
-        self.sigma2 = sigma2
+        self.sigma = sigma
 
     def __call__(self, batch):
         """
@@ -44,7 +47,7 @@ class DiscreteMMSE:
             data: batch_size x n_points x n_dims (float)
             targets: batch_size x n_points (float)
         Return:
-            batch_size x n_points (float)
+            batch_size x n_points (float) OR batch_size x 1 IF index is not None
         """
         data = batch["xs"]
         targets = batch["ys"]
@@ -75,7 +78,7 @@ class DiscreteMMSE:
             batch_size (float)
         """
         # X @ W is batch_size x i x n_tasks, Y is batch_size x i x 1, so broadcasts to alpha being batch_size x n_tasks
-        alpha = -torch.sum((Y - torch.matmul(X, W))**2, axis=1) / (2*self.sigma2)
+        alpha = -torch.sum((Y - torch.matmul(X, W))**2, axis=1) / (2*self.sigma**2)
         # softmax is batch_size x n_tasks, W.T is n_tasks x n_dims, so w_mmse is batch_size x n_dims x 1
         w_mmse = torch.unsqueeze(torch.matmul(torch.nn.Softmax(dim=1)(alpha), W.T), -1).float()
         # test_x is batch_size x 1 x n_dims, so pred is batch_size x 1 x 1. NOTE: @ should be ok (batched row times column)
